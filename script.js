154df17ad851f9fea8ac3fef7ff69bbfe8c8b96b
Gackun1 <gaku090707@gmail.com>
2021-06-11 08:08:36 +0900

    基本機能の実装

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
 document.addEventListener("DOMContentLoaded", () => {
   const inputForm = document.getElementById("input-form");
   const outputForm = document.getElementById("output-form");
-  const btn = document.getElementById("conversion-btn");
+  const conversionBtn = document.getElementById("conversion-btn");
+  const optionBtn = document.getElementById("option-btn");
+  const copyBtn = document.getElementById("copy-btn");
   const option = {
-    className: {
-      p: "",
-      h1: "",
-      h2: "",
-      h3: "",
-      h4: "",
-      h5: "",
-    },
-    isKeepEmptyLine: true,
+    isKeepEmptyLine: false,
     replaceTagEmptyLine: "<br>",
+    wrapTagLine: "<p>",
   };
 
-  btn.addEventListener("click", () => {
-    const inputText = inputForm.value;
-    let outputText = "";
+  const setOption = () => {
+    const keepEmptyLine = document.getElementById("keep-empty-line");
+    const replaceTag = document.getElementById("replace-tag-empty-line");
+    const wrapTag = document.getElementById("wrap-tag-line");
+    option.isKeepEmptyLine = keepEmptyLine.checked;
+    option.replaceTagEmptyLine = replaceTag.value;
+    option.wrapTagLine = wrapTag.value;
+  };
 
+  const createEndTag = (str) => {
+    const endTag = [];
+    for (let value of [...str]) {
+      if ((value === " ") | (value === ">")) {
+        break;
+      }
+      endTag.push(value);
+    }
+    endTag.splice(1, 0, "/");
+    endTag.push(">");
+
+    return endTag.join("");
+  };
+
+  conversionBtn.addEventListener("click", () => {
+    let outputText = "";
+    const inputText = inputForm.value;
+    const startTag = option.wrapTagLine;
+    const endTag = createEndTag(option.wrapTagLine);
     const lines = inputText.split(/\r\n|\n/);
+
     lines.forEach((v) => {
       if (v.match(/\S/g)) {
-        outputText += `<p>${v}</p>\n`;
+        outputText += `${startTag}${v}${endTag}\n`;
       } else if (option.isKeepEmptyLine) {
         outputText += `${option.replaceTagEmptyLine}\n`;
       }
     });
-    console.log(inputText, outputText);
 
     outputForm.value = outputText;
+  });
+
+  optionBtn.addEventListener("click", setOption);
+
+  copyBtn.addEventListener("click", () => {
+    //select textarea
+    outputForm.select();
+
+    //copy on clipboard
+    document.execCommand("copy");
+
+    //remove all selection
+    window.getSelection().removeAllRanges();
+
+    //show message
+    const msg = document.getElementById("copy-msg");
+    msg.classList.remove("show");
+    setTimeout(() => msg.classList.add("show"), 1);
   });
 });
